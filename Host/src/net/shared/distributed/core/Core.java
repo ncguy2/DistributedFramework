eb2d3ef035612dd14b7ce1256342f29a7f773472
ncguy2 <nick.guy@hotmail.co.uk>
2017-05-16 18:32:41 +0100

    Commit of Basic structure

---
+++ Host/src/net/shared/distributed/core/Core.java
@@ -0,0 +1,7 @@
+package net.shared.distributed.core;
+
+/**
+ * Created by Guy on 16/05/2017.
+ */
+public class Core {
+}
---
+++ Host/src/net/shared/distributed/distributor/Distributor.java
@@ -0,0 +1,7 @@
+package net.shared.distributed.distributor;
+
+/**
+ * Created by Guy on 16/05/2017.
+ */
+public class Distributor {
+}
